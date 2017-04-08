67e144c96869d17c1099055610bea189fb86df63
Bryce <brycemwilson@gmail.com>
2017-04-07 19:29:25 -0500

    Add memcpy utility procedure

--- util.asm
+++ util.asm
@@ -13,3 +13,48 @@
 
 	rts
 .endproc
+
+;--------------------------------------
+; memmove moves zp::tmp0 bytes from (zp::tmp2) to (zp::tmp4).
+.export __util_memcpy
+.proc __util_memcpy
+	ldy #$00
+
+	lda zp::tmp4
+	clc
+	adc zp::tmp0
+	sta zp::tmp4
+	lda zp::tmp4+1
+	adc zp::tmp0+1
+	sta zp::tmp4+1
+	
+	lda zp::tmp2
+	clc
+	adc zp::tmp0
+	sta zp::tmp2
+	lda zp::tmp2+1
+	adc zp::tmp0+1
+	sta zp::tmp2+1
+
+	ldy #$00
+@l0:	lda (zp::tmp2),y
+	sta (zp::tmp4),y
+
+	lda zp::tmp2
+	bne :+
+	dec zp::tmp2+1
+:	dec zp::tmp2
+
+	lda zp::tmp4
+	bne :+
+	dec zp::tmp4+1
+:	dec zp::tmp4
+
+	lda zp::tmp0
+	bne :+
+	dec zp::tmp0+1
+	bpl :+
+	rts
+:	dec zp::tmp0
+	jmp @l0
+.endproc
