4e37cbe987dee62b95535d7aa2e06ac9976b5927
Bryce Wilson <brycemwilson@gmail.com>
2025-02-08 09:54:12 -0500

    Refactor platform independent cursor code

---
+++ src/cursor.asm
@@ -0,0 +1,164 @@
+.include "zeropage.inc"
+
+.import __cur_toggle
+
+;*******************************************************************************
+.BSS
+.export __cur_mode
+__cur_mode: .byte 0	; 0 = NORMAL, 1 = SELECT
+
+.export __cur_status
+__cur_status: .byte 0	; 0 = OFF, 1 = ON
+
+.export __cur_minx
+__cur_minx:
+minx: .byte 0
+
+.export __cur_maxx
+__cur_maxx:
+maxx: .byte 0
+
+.export __cur_miny
+__cur_miny:
+miny: .byte 0
+
+.export __cur_maxy
+__cur_maxy:
+maxy: .byte 0
+
+.CODE
+
+;*******************************************************************************
+; OFF
+; Turns off the cursor if it is on. Has no effect if the cursor is already off.
+.export __cur_off
+.proc __cur_off
+	lda __cur_status
+	beq :++
+:	jmp __cur_toggle
+.endproc
+
+;*******************************************************************************
+; ON
+; Turns on the cursor if it is off. Has no effect if the cursor is already on.
+.export __cur_on
+.proc __cur_on
+	lda __cur_mode
+	bne :-			; always turn ON in SELECT mode
+	lda __cur_status
+	bne :+
+	jmp __cur_toggle
+:	rts
+.endproc
+
+;*******************************************************************************
+; MOVE
+; Updates the cursor's (x,y) position by the offsets given
+; IN:
+;  - .X: the signed number of columns to move
+;  - .Y: the signed number of rows to move
+.export __cur_move
+.proc __cur_move
+@xdst=r2
+@ydst=r3
+	stx @xdst
+	sty @ydst
+	lda __cur_mode
+	bne :+
+	jsr __cur_off
+:	lda @xdst
+	clc
+	adc zp::curx
+	bmi @movey
+	cmp maxx
+	bcs @movey
+	cmp minx
+	bcc @movey
+	sta zp::curx
+
+@movey: lda @ydst
+	clc
+	adc zp::cury
+	bmi @done
+	cmp maxy
+	bcs @done
+	cmp miny
+	bcc @done
+	sta zp::cury
+@done:	rts
+.endproc
+
+;*******************************************************************************
+; SET
+; Sets the cursor position (x,y) to the values given
+; IN:
+;  .X: the column to set the cursor to
+;  .Y: the row to set the cursor to
+.export __cur_set
+.proc __cur_set
+	cpx maxx
+	bcc :+
+	ldx maxx
+	dex
+:	cpx minx
+	bcs :+
+	ldx minx
+:	stx r2
+
+	cpy maxy
+	bcc :+
+	ldy maxy
+	dey
+:	cpy miny
+	bcs :+
+	ldy miny
+:	sty r3
+
+	jsr __cur_off
+
+	ldx r2
+	ldy r3
+	stx zp::curx
+	sty zp::cury
+	rts
+.endproc
+
+;*******************************************************************************
+; UNLIMIT
+.export __cur_unlimit
+.proc __cur_unlimit
+	ldx #$00
+	stx minx
+	stx miny
+
+	ldx #40
+	ldy #23
+
+	; fall through to __cur_setmax
+.endproc
+
+;*******************************************************************************
+; SETMAX
+; Sets the maximum values for the cursor's X and Y values
+; IN:
+;  - .X: the column limit to set for the cursor
+;  - .Y: the row limit to set for the cursor
+.export __cur_setmax
+.proc __cur_setmax
+	stx maxx
+	sty maxy
+	rts
+.endproc
+
+;*******************************************************************************
+; SETMIN
+; Sets the minimum values for the cursor's X and Y values
+; IN:
+;  - .X: the column limit to set for the cursor
+;  - .Y: the row limit to set for the cursor
+.export __cur_setmin
+.proc __cur_setmin
+	stx minx
+	sty miny
+	rts
+.endproc
