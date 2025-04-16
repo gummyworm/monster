7bd1faa75569a0e6191f555550c4ec829e8db6e9
Bryce Wilson <brycemwilson@gmail.com>
2025-04-16 15:44:06 -0500

    Add build script

---
+++ src/make-release.sh
@@ -0,0 +1,23 @@
+make clean
+
+# make the installer
+make installer.prg
+cp installer.prg ../bin/
+
+# make the NTSC cart binary
+make clean; make cart REGION=NTSC
+cp monster.bin ../bin/monster-ntsc.bin
+
+# make the PAL cart binary
+make clean; make cart REGION=PAL
+cp monster.bin ../bin/monster-pal.bin
+
+# make the NTSC disk image
+make clean; make disk REGION=NSTC
+c1541 -format monster,1 d81 monster.d81 -attach monster.d81 -write boot.prg -write masm.prg
+cp monster.d81 ../bin/monster-ntsc.d81
+
+# make the PAL disk image
+make clean; make disk REGION=PAL
+c1541 -format monster,1 d81 monster.d81 -attach monster.d81 -write boot.prg -write masm.prg
+cp monster.d81 ../bin/monster-pal.d81
